5881e91ac80a08d060cb6ac3b8634d3fad2cece7
Harry Howarth <harryehowarth@gmail.com>
2019-04-07 19:42:00 +0100

    Changed the secret

--- springboot-main-app/src/main/java/com/howarth/cloud/mainapp/security/SecurityConstants.java
+++ springboot-main-app/src/main/java/com/howarth/cloud/mainapp/security/SecurityConstants.java
@@ -3,7 +3,7 @@ package com.howarth.cloud.mainapp.security;
 import org.springframework.http.HttpMethod;
 
 public class SecurityConstants {
-    public static final String SECRET = "SecretKeyToGenJWTs";
+    public static final String SECRET = "Team17sFilthyLittleSecret";
     public static final long EXPIRATION_TIME = 864_000_000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
