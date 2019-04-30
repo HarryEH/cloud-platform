2a2b9a02ca740a51cbe0119b50737d751297cf11
Niall <nstevenson1@sheffield.ac.uk>
2019-04-30 09:50:34 +0100

    integrated into main app

--- grade-calculator/src/main/java/com/stevenson/grades/database/GradeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stevenson.grades.database;
-
-import com.stevenson.grades.database.model.Grade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GradeRepository extends JpaRepository<Grade, Long> {
-}
--- grade-calculator/src/main/java/com/stevenson/grades/database/UserModuleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stevenson.grades.database;
-
-import com.stevenson.grades.database.model.UserModule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserModuleRepository extends JpaRepository<UserModule, Long> {
-    UserModule findDistinctByUserID(long userID);
-}
