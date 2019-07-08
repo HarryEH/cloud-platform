db0a0d6a3126864db691ade032da6aaafc6259fc
Harry <harryehowarth@gmail.com>
2019-07-08 12:32:08 +0100

    cleans up the repository, removing useless directories

--- library-recommendation-app/src/main/java/com/howarth/cloud/mainapp/library/security/SecurityConstants.java
+++
@@ -1,8 +0,0 @@
-package com.howarth.cloud.mainapp.library.security;
-
-public class SecurityConstants {
-    public static final String SECRET = "SecretKeyToGenJWTs";
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-    public static final String COOKIE = "Cookie";
-}
--- springboot-main-app/src/main/java/com/howarth/cloud/mainapp/grades/database/UserModuleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.howarth.cloud.mainapp.grades.database;
-
-import com.howarth.cloud.mainapp.grades.database.model.UserModule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserModuleRepository extends JpaRepository<UserModule, Long> {
-    UserModule findDistinctByUsername(String username);
-}
